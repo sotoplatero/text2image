faf988e10aecac68e771bb74ccb45f875b07a739
Damian Soto <soto.platero@gmail.com>
2025-09-23 06:41:38 -0400

    add favicon

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,8 +2,8 @@
 	<div class="container mx-auto max-w-4xl">
 		<div class="flex flex-col lg:flex-row items-start lg:items-center gap-4">
 			<div class="flex-shrink-0">
-				<h1 class="text-xl font-bold text-gray-900 dark:text-gray-100">
-					Text to Image
+				<h1 class="text-xl font-bold text-gray-900 dark:text-gray-100 tracking-widest">
+					screenshotext
 				</h1>
 			</div>
 		</div>
